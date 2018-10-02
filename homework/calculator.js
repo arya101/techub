bcd4b9d9406c865a56ef055aa402b31feef2c0d0
tsilagava <34042338+tsilagava@users.noreply.github.com>
2018-10-03 02:17:58 +0400

    Add files via upload

---
+++ homework/calculator.js
@@ -0,0 +1,21 @@
+let a=Number(prompt("enter first number"));
+let b=Number(prompt("enter second number"));
+let oper = prompt("enter operation");
+switch (oper) {
+    case '+':
+    alert(a+b);
+    break;
+
+    case '-' :
+    alert(a-b);
+    break;
+
+    case '*' :
+    alert(a*b);
+    break;
+    
+    case '/' :
+    alert(a/b);
+    break;
+
+}
